77a368f8920b2a8c59475209596f5feb07929927
Summon Yang <SummonYang1991@gmail.com>
2015-06-03 12:19:01 +0800

    Initial Commit

---
+++ ComChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComChat
+//
+//  Created by D404 on 15/6/3.
+//  Copyright (c) 2015年 D404. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ComChat
+//
+//  Created by D404 on 15/6/3.
+//  Copyright (c) 2015年 D404. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
